8680e54b7c094c72a8db7078c4b498d5d938961b
Keerthan K <79328813+keerthnn@users.noreply.github.com>
2023-01-24 16:55:59 +0530

    Update app.py

--- project/app.py
+++ project/app.py
@@ -1,6 +1,7 @@
-from flask import Flask, request, render_template, request, redirect, url_for, session
+from flask import Flask, request, render_template, request, redirect, url_for, session ,flash
 from flask_mysqldb import MySQL
 import MySQLdb.cursors
+from functools import wraps
 
 app=Flask(__name__)
 
@@ -13,9 +14,13 @@ app.config['MYSQL_DB'] = 'gcms'
 
 mysql = MySQL(app)
 
+# -----------------------------------------------------------------------------------------------------------------------
+
 @app.route('/')
 def home():
     return render_template('home.html')
+
+# -----------------------------------------------------------------------------------------------------------------------
 
 
 @app.route('/flogin', methods=['POST','GET'])
@@ -37,22 +42,29 @@ def flogin():
             session['fid'] = account['fid']
             session['username'] = account['username']
             mesage = 'Logged in successfully !'
-            return render_template('user.html', mesage = mesage)
+            # flash('Logged in successfully !')
+            return render_template('fdashboard.html', mesage = mesage)
 
         elif not username or not password :
             mesage = 'Please fill out the form !'
         else:
             
             mesage = 'Incorrect username / password !'
-            return  render_template('home.html', mesage = mesage)
+            return  render_template('flogin.html', mesage = mesage)
     return render_template('flogin.html')
+
+# -----------------------------------------------------------------------------------------------------------------------
+
 
 @app.route('/logout')
 def logout():
     session.pop('loggedin', None)
     session.pop('fid', None)
-    session.pop('email', None)
+    session.pop('username', None)
     return redirect(url_for('home'))
+
+
+# -----------------------------------------------------------------------------------------------------------------------
 
 @app.route('/fregister', methods=['POST','GET'])
 def fregister():
@@ -89,6 +101,10 @@ def fregister():
     return  render_template('fregister.html', mesage = mesage)
     
 
+
+# -----------------------------------------------------------------------------------------------------------------------
+
+
 @app.route('/gclogin', methods=['POST','GET'])
 def gclogin():
 
@@ -108,7 +124,7 @@ def gclogin():
             session['gid'] = account['gid']
             session['username'] = account['username']
             mesage = 'Logged in successfully !'
-            return render_template('user.html', mesage = mesage)
+            return render_template('gcdashboard.html', mesage = mesage)
 
         elif not username or not password :
             mesage = 'Please fill out the form !'
@@ -117,6 +133,9 @@ def gclogin():
             mesage = 'Incorrect username / password !'
             return  render_template('home.html', mesage = mesage)
     return render_template('gclogin.html')
+
+
+# -----------------------------------------------------------------------------------------------------------------------
 
 
 @app.route('/gcregister', methods=['POST','GET'])
@@ -129,12 +148,13 @@ def gcregister():
     if request.method == 'GET':
         return  render_template('gcregister.html')
      
-    if request.method == 'POST' and 'name' in request.form and 'address' in request.form and 'phone' in request.form and 'username' in request.form and 'password' in request.form:
+    if request.method == 'POST' and 'name' in request.form and 'address' in request.form and 'phone' in request.form and 'username' in request.form and 'password' in request.form and 'charges' in request.form:
         name = request.form['name']
         address = request.form['address']
         phone = request.form['phone']
         username = request.form['username']
         password = request.form['password']
+        charges = request.form['charges']
 
         cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
         cursor.execute('SELECT * FROM GRASS_CUTTER WHERE username = % s ', (username, ))
@@ -144,7 +164,7 @@ def gcregister():
         elif not username or not password :
             mesage = 'Please fill out the form !'
         else:
-            cursor.execute(' INSERT INTO GRASS_CUTTER VALUES(NULL, %s , %s , %s , %s ,%s)',(name,address,phone,username,password))
+            cursor.execute(' INSERT INTO GRASS_CUTTER VALUES(NULL, %s , %s , %s , %s ,%s,%s)',(name,address,phone,username,password,charges))
             mesage = 'You have successfully registered !'
         mysql.connection.commit()
         cursor.close()
@@ -152,6 +172,114 @@ def gcregister():
     elif request.method == 'POST':
         mesage = 'Please fill out the form !'
     return  render_template('gcregister.html', mesage = mesage)
+
+
+
+# -----------------------------------------------------------------------------------------------------------------------
+
+
+@app.route('/fdashboard')
+def fdashboard():
+
+    cursor = mysql.connection.cursor()
+    cursor.execute("SELECT * FROM GRASS_CUTTER")
+    data=cursor.fetchall()
+    cursor.close()
+
+    
+    return  render_template('fdashboard.html', data=data)
+
+
+
+# -----------------------------------------------------------------------------------------------------------------------
+
+
+
+@app.route('/booking',methods=['POST','GET'])
+def booking():
+
+    print("=========================================")
+    val2 = [j for j in request.form.values()]
+    print(val2)
+    
+    mesage = ''
+    if request.method == 'GET':
+        return  render_template('booking.html')
+     
+    if request.method == 'POST' and 'fname' in request.form and 'address' in request.form and 'phone' in request.form and 'gid' in request.form:
+        fname = request.form['fname']
+        address = request.form['address']
+        phone = request.form['phone']
+        gid = request.form['gid']
+
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute('SELECT * FROM BOOKING WHERE fname = % s ', (fname, ))
+        account = cursor.fetchone()
+        if account:
+            mesage = 'already booked !'
+        else:
+            cursor.execute(' INSERT INTO BOOKING VALUES(NULL, %s , %s , %s, %s)',(fname,address,phone,gid))
+            mesage = 'You have successfully booked !'
+        mysql.connection.commit()
+        cursor.close()
+        return  render_template('fdashboard.html', mesage = mesage)
+    elif request.method == 'POST':
+        mesage = 'Please fill out the form !'
+    return  render_template('fdashboard.html', mesage = mesage)
+
+# -----------------------------------------------------------------------------------------------------------------------
+
+
+
+@app.route('/gcdashboard')
+def gcdashboard():
+
+    cursor = mysql.connection.cursor()
+    cursor.execute("""SELECT * from BOOKING where gid={0}""".format(session['gid']))
+    data=cursor.fetchall()
+    cursor.execute("""SELECT * from REPAIR_SHOP where gid={0}""".format(session['gid']))
+    result=cursor.fetchall()
+    cursor.close()
+    return  render_template('gcdashboard.html', data=data,result=result)
+
+# -----------------------------------------------------------------------------------------------------------------------
+
+@app.route('/shop',methods=['POST','GET'])
+def shop():
+
+    print("=========================================")
+    val2 = [j for j in request.form.values()]
+    print(val2)
+    
+    mesage = ''
+    if request.method == 'GET':
+        return  render_template('shop.html')
+     
+    if request.method == 'POST' and 'shop_name' in request.form and 'tools' in request.form and 'tool_cost' in request.form and 'gid' in request.form:
+        shop_name = request.form['shop_name']
+        tools = request.form['tools']
+        tool_cost = request.form['tool_cost']
+        gid = request.form['gid']
+
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute('SELECT * FROM REPAIR_SHOP WHERE tools = % s ', (tools, ))
+        account = cursor.fetchone()
+        if account:
+            mesage = 'already bought !'
+        else:
+            cursor.execute(' INSERT INTO REPAIR_SHOP VALUES( %s , %s , %s, %s)',(shop_name,tools,tool_cost,gid))
+            mesage = 'You have successfully bought !'
+        mysql.connection.commit()
+        cursor.close()
+        return  render_template('gcdashboard.html', mesage = mesage)
+    elif request.method == 'POST':
+        mesage = 'Please fill out the form !'
+    return  render_template('gcdashboard.html', mesage = mesage)
+
+
+
+
+# -----------------------------------------------------------------------------------------------------------------------
 
 if __name__=="__main__":
     app.run(debug=True)
